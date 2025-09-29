8a5df4fc0ddc039631aba1d9ede90f56dd82c229
Dennis Jedidiah <thejedidiahdennis@gmail.com>
2025-09-28 22:16:32 -0400

    change font

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <div className="Construction_sign">
-        <h1>Page Under Construction</h1>
+        <h1 className="zalando-sans-expanded">Page Under Construction</h1>
         <p id='subheading'>We're working hard to get this page up and running. Stay tuned!</p>
       </div>
     </>
