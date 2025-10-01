d72e719516dbd8ae833094dbb7e7bd3d90219b54
Dennis Jedidiah <thejedidiahdennis@gmail.com>
2025-09-30 23:19:24 -0400

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Projects />
       <Contact />
       <Skills />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { LinkedIn } from "../assets/icons/export";
+
+const Footer = () => {
+    return ( 
+        <div className="footer" id="footer">
+            <p>Built with React, Deployed on Vercel</p>
+            <div style={{ border: '0.5px solid #524F7D', height: '0px', width:'70%' }} />
+            <p>© 2025 Dennis Jedidiah</p>
+        </div>
+     );
+}
+ 
+export default Footer;
